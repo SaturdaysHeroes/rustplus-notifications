e244f2568f3644dadea29eaf38635f9fe5f25f33
Oskar <saturdaysheroes.business@gmail.com>
2021-01-06 00:00:44 +0100

    send a notification on alarm trigger

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,29 @@ import {Rust} from "./rust";
 
 Rust.on("connected", () => {
     console.log("[Rust+] Successfully connected.");
+
     Discord({
         title: "Connected", 
         description: "Rust+ has successfully been connected."
     });
-});
 
-Rust.on("message", (message) => {
-    if (process.env.DEBUG == "ON") {
-        console.log("[Rust+] Received message: " + JSON.stringify(message));
-    }
+    Rust.getEntityInfo(process.env.RUST_ALARMID, () => {
+        console.log("[Rust+] Successfully requested information about entites.");
+    });
+
+    Rust.on("message", (message) => {
+        if (message.broadcast && message.broadcast.entityChanged) {
+            let entityChanged = message.broadcast.entityChanged;
+            let entityID = entityChanged.entityId; 
+            
+            if (entityID == process.env.RUST_ALARMID) {
+                Discord({
+                    title: "Alarm Triggered",
+                    description: "An intruder has been detected by your smart alarm!"
+                });
+
+                console.log("[Rust+] Alarm triggered, notificaton sent.")
+            };
+        }
+    });
 });
