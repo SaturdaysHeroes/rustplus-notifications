5efef08e577d4349826b744992208eac9471cec0
Oskar <saturdaysheroes.business@gmail.com>
2021-01-05 21:24:50 +0100

    notify discord on connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+import * as dotenv from "dotenv"; dotenv.config();
+import {Discord} from "./discord";
+import {Rust} from "./rust";
+
+Rust.on("connected", () => {
+    console.log("[Rust+] Successfully connected.");
+    Discord({
+        title: "Connected", 
+        description: "Rust+ has successfully been connected."
+    });
+});
